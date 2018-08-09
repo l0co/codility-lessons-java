0171ae4ca171ef2173bd554b953e3d9c7be492c7
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-09 12:48:17 +0200

    lesson 04

---
+++ src/e04/missingInteger/Solution.java
@@ -0,0 +1,29 @@
+package e04.missingInteger;
+
+// https://app.codility.com/demo/results/trainingNFW285-8RT/
+public class Solution {
+
+	public int solution(int[] a) {
+		int occurrences[] = new int[a.length];
+		int i=0;
+		for (i=0; i<a.length; i++) {
+			int item = a[i];
+			if (a[i]>=1 && a[i]<=a.length)
+				occurrences[a[i]-1]++;
+		}
+		for (i=0; i<occurrences.length; i++) {
+			if (occurrences[i]==0)
+				return i+1;
+		}
+
+		return i+1;
+	}
+
+	public static void main(String [] args) {
+		System.out.println(new Solution().solution(new int[] {1, 3, 6, 4, 1, 2}));
+		System.out.println(new Solution().solution(new int[] {1, 2, 3}));
+		System.out.println(new Solution().solution(new int[] {-1, -3}));
+		System.out.println(new Solution().solution(new int[] {1, 3, 6, 4, 1, 2, 100000}));
+	}
+
+}
