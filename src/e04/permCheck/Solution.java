f45d523491e5a564374192cd4070a033c3213de3
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-09 12:00:28 +0200

    lesson 04

---
+++ src/e04/permCheck/Solution.java
@@ -0,0 +1,29 @@
+package e04.permCheck;
+
+// https://app.codility.com/demo/results/trainingNGBENQ-ER3/
+public class Solution {
+
+	public int solution(int[] a) {
+		int[] b = new int[a.length];
+		for (int i=0; i<a.length; i++) {
+			int val = a[i];
+			if (val > a.length) // value outside boundary
+				return 0;
+			if (b[val-1] != 0) // value exists again
+				return 0;
+			b[val-1] = 1;
+		}
+		return 1;
+	}
+
+	public static void main(String [] args) {
+		System.out.println(new Solution().solution(new int[] {4, 1, 3, 2}));
+		System.out.println(new Solution().solution(new int[] {4, 1, 3}));
+		System.out.println(new Solution().solution(new int[] {2, 1}));
+		System.out.println(new Solution().solution(new int[] {2}));
+		System.out.println(new Solution().solution(new int[] {1}));
+		System.out.println(new Solution().solution(new int[] {5, 1, 3, 2, 4, 6, 7}));
+		System.out.println(new Solution().solution(new int[] {}));
+	}
+
+}
