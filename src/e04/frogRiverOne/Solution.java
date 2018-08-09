90bb2cef5ae7c6b27d3163cf02ec05de60e57994
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-09 12:20:40 +0200

    lesson 04

---
+++ src/e04/frogRiverOne/Solution.java
@@ -0,0 +1,30 @@
+package e04.frogRiverOne;
+
+// https://app.codility.com/demo/results/trainingPSH3EB-SXQ/
+public class Solution {
+
+	public int solution(int len, int[] times) {
+		int positions[] = new int[len];
+		int positionsCovered = 0;
+
+		for (int second=0; second<times.length; second++) {
+			int position = times[second];
+			if (position > positions.length)
+				throw new IllegalArgumentException(String.format("Position: %d at second: %d greater than len: %d", position, second, len));
+			if (positions[position-1]==0) { // record new leaf on position
+				positions[position-1] = 1;
+				positionsCovered++;
+				if (positionsCovered==len) // all positions covered? return time
+					return second;
+			}
+
+		}
+
+		return -1;
+	}
+
+	public static void main(String [] args) {
+		System.out.println(new Solution().solution(5, new int[] {1, 3, 1, 4, 2, 3, 5, 4}));
+	}
+
+}
