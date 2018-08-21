78c80e92d76235f620cccc94fa2351c45effbec7
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-21 12:55:57 +0200

    MinAvgTwoSlice 100%

--- src/e05/MinAvgTwoSlice.java
+++ src/e05/MinAvgTwoSlice.java
@@ -1,68 +1,38 @@
 package e05;
 
-import java.util.ArrayList;
-import java.util.List;
-
 // https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
-// wrong answer 40% (this is a wrong assumption that minimal value will always be involved into minimal average - consider [2, 2, 9, 1, 9])
-// https://app.codility.com/demo/results/trainingC89822-GGG/
+// this solution is a bit tricy, we calculate min avg for pairs, then we calculate min avg for tripples, if tripples has lesser avg then
+// pairs, we then calculate min for fours and check again agains tripples; if tripples doesn't have lesser avg, pairs are winning
+// https://app.codility.com/demo/results/trainingWGU5MN-K7W/
 public class MinAvgTwoSlice {
 
 	public int solution(int[] a) {
-		List<Integer> minValIdx = new ArrayList<>();
-		int minVal = Integer.MAX_VALUE;
-
-		// find indices of minimal value in the array
-		for (int i=0; i<a.length; i++) {
-			if (minVal>a[i]) {
-				minVal = a[i];
-				minValIdx.clear();;
-			}
-			if (minVal==a[i])
-				minValIdx.add(i);
-		}
-
-
+		int step = 1;
 		double minAvg = Double.MAX_VALUE;
 		int minAvgIdx = 0;
 
-		// for each minimal value find the minimal average going left and right
-		for (int i: minValIdx)
-			if (i>0) {
-				double leftAvg = a[i];
-				int left = i;
-				while (--left > 0) {
-					double localLeftAvg = (leftAvg + a[left]) / 2;
-					if (localLeftAvg<leftAvg || left==i-1) {
-						leftAvg = localLeftAvg;
-						if (leftAvg<minAvg) {
-							minAvg = leftAvg;
-							minAvgIdx = left;
-						}
-					} else {
-						break;
-					}
+		while (++step < a.length) {
+
+			double localMinAvg = Double.MAX_VALUE;
+			int localMinAvgIdx = 0;
+			for (int i=0; i<=a.length-step; i++) {
+				double sum = 0;
+				for (int j=0; j<step; j++)
+					sum += a[i+j];
+				double avg = sum / step;
+				if (avg<localMinAvg) {
+					localMinAvg = avg;
+					localMinAvgIdx = i;
 				}
 			}
 
-		for (int i: minValIdx)
-			if (i<a.length-1) {
-				double rightAvg = a[i];
-				int right = i;
-				while (++right < a.length) {
-					double localRightAvg = (rightAvg + a[right]) / 2;
-					if (localRightAvg<rightAvg || right==i+1) {
-						rightAvg = localRightAvg;
-						if (rightAvg<minAvg) {
-							minAvg = rightAvg;
-							minAvgIdx = i;
-							break;
-						}
-					} else {
-						break;
-					}
-				}
-			}
+			if (localMinAvg<minAvg) {
+				minAvg = localMinAvg;
+				minAvgIdx = localMinAvgIdx;
+			} else
+				break; // no better min avg found in the iteration, stop
+
+		}
 
 		return minAvgIdx;
 	}
@@ -70,7 +40,8 @@ public class MinAvgTwoSlice {
 	public static void main(String [] args) {
 		System.out.println(new MinAvgTwoSlice().solution(new int[] {4, 2, 2, 5, 1, 5, 8})); // 1->2 (1)
 		System.out.println(new MinAvgTwoSlice().solution(new int[] {-3, -5, -8, -4, -10})); // 2->4 (2)
-		System.out.println(new MinAvgTwoSlice().solution(new int[] {-3, -5, -8, -4, -10, -9})); // 4=>5 (4)
+		System.out.println(new MinAvgTwoSlice().solution(new int[] {-3, -5, -8, -4, -10, -9})); // 4->5 (4)
+		System.out.println(new MinAvgTwoSlice().solution(new int[] {-1000, 1000})); // 0->1 (0) -- boundary condition
 	}
 
 }
