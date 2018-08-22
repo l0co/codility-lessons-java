ae8b3cb2a4ac6fd493f3309e4f2d18f35711e350
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-22 12:37:53 +0200

    avoid creating subarrays in merge sort

--- src/e06/MergeSort.java
+++ src/e06/MergeSort.java
@@ -6,30 +6,39 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public int[] solution(int[] a) {
-		return sort(a);
+		return sort(a, 0, a.length);
 	}
 
-	protected int[] sort(int[] a) {
-		if (a.length==1)
+	protected int[] sort(int[] a, int leftInclusive, int rightExclusive) {
+		int len = rightExclusive-leftInclusive;
+		if (len<=1)
 			return a;
 
-		int half = a.length / 2;
-		return merge(sort(Arrays.copyOfRange(a, 0, half)), sort(Arrays.copyOfRange(a, half, a.length)));
+		int half = len / 2;
+		sort(a, leftInclusive, leftInclusive+half);
+		sort(a, leftInclusive+half, leftInclusive+len);
+		return merge(a, leftInclusive, leftInclusive+half, leftInclusive+len);
 	}
 
-	protected int[] merge(int[] a, int[] b) {
-		int[] merged = new int[a.length+b.length];
-		int aidx=0, bidx=0;
-		while (aidx<a.length || bidx<b.length) {
-			if (aidx==a.length || (bidx<b.length && b[bidx]<=a[aidx]))
-				// b element is lesser or no more a elements
-				merged[aidx+bidx] = b[bidx++];
-			else if (bidx==b.length || a[aidx]<=b[bidx])
-				// a element is lesser or no more b elements
-				merged[aidx+bidx] = a[aidx++];
+	protected int[] merge(int[] a, int leftInclusive, int half, int rightExclusive) {
+		int len = rightExclusive-leftInclusive;
+		int[] merged = new int[len];
+		int aidx=leftInclusive, bidx=half, midx=0;
+		while (aidx<half || bidx<rightExclusive) {
+			if (aidx==half || (bidx<rightExclusive && a[bidx]<=a[aidx]))
+				merged[midx++] = a[bidx++];
+			else if (bidx==rightExclusive || a[aidx]<=a[bidx])
+				merged[midx++] = a[aidx++];
 		}
 
-		System.out.println(String.format("Merging: %s and: %s into: %s", Arrays.toString(a), Arrays.toString(b), Arrays.toString(merged)));
+		System.out.println(String.format("Merging: %s and: %s into: %s",
+			Arrays.toString(Arrays.copyOfRange(a, leftInclusive, half)),
+			Arrays.toString(Arrays.copyOfRange(a, half, rightExclusive)),
+			Arrays.toString(merged)));
+
+		// fill merged into the original array
+		for (int i=0; i<merged.length; i++)
+			a[leftInclusive+i] = merged[i];
 
 		return merged;
 	}
