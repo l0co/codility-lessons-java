888424a3c84fcdbf035eca4ab2698f4fa14060b8
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-08-23 10:15:53 +0200

    trianle 93% but unknown test case for bad answer

---
+++ src/e06/Triangle.java
@@ -0,0 +1,33 @@
+package e06;
+
+import java.util.Arrays;
+
+// https://app.codility.com/programmers/lessons/6-sorting/triangle/
+// 93% (wrong answer) https://app.codility.com/demo/results/trainingC7CKNP-N4C/
+public class Triangle {
+
+	public int solution(int[] arr) {
+		Arrays.sort(arr);
+
+		int a,b,c;
+
+		for (int i=0; i<arr.length-2; i++) {
+			a = arr[i];
+			b = arr[i+1];
+			int cLesserThan = a + b;
+			int cGreaterThan = Math.max(a-b, b-a);
+
+//			System.out.println(String.format("Checking: [%d, %d] -> 1)  -->  c < %d  &&  c > %d", a, b, cLesserThan, cGreaterThan));
+			int j = i+1;
+			while (++j<arr.length && arr[j]<cLesserThan && arr[j]>cGreaterThan)
+				return 1;
+		}
+
+		return 0;
+	}
+
+	public static void main(String [] args) {
+		System.out.println(new Triangle().solution(new int[] {10, 2, 5, 1, 8, 20}));
+	}
+
+}
