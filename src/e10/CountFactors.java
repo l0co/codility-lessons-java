81a26ecfa29a85ad9c966fffa77f39bf69cfaef8
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-09-24 07:05:10 +0200

    count factors - 100% performance

--- src/e10/CountFactors.java
+++ src/e10/CountFactors.java
@@ -2,27 +2,29 @@ package e10;
 
 /**
  * Task: https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/count_factors/
- * Solution: https://app.codility.com/demo/results/trainingAFSTTT-DCM/
- * Result: 92% because of performance
+ * Solution: https://app.codility.com/demo/results/trainingKNGXTJ-M7U/
+ * Result: 100%
  *
  * @author Lukasz Frankowski
  */
 public class CountFactors {
 
 	public int solution(int n) {
-		int i = 1, count = 0;
+		int i = 0, count=0;
 
-		while (i*i < n) {
-			if (n % i == 0)
-				count += 2;
-			i++;
-		}
+		double sqrt = Math.sqrt(n);
+		int isqrt = (int) Math.floor(sqrt);
 
-		return i*i==n ? count+1 : count;
+		while (++i <= isqrt)
+			count += n % i == 0 ? (i==sqrt ? 1 : 2) : 0;
+
+		return count;
 	}
 
 	public static void main(String [] args) {
 		System.out.println(new CountFactors().solution(24)); // 8
+		System.out.println(new CountFactors().solution(36)); // 9
+		System.out.println(new CountFactors().solution(2147483647)); // 2
 	}
 
 }
